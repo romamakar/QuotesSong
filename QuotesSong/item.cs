152d016c3e20c5cc90c20c63bd79cfc1ee5a38e7
roman <roman.makarevych@gmail.com>
2016-12-29 16:48:44 +0200

    init version

--- QuotesSong/item.cs
+++ QuotesSong/item.cs
@@ -1,12 +1,11 @@
 ﻿using System.Xml.Serialization;
-
+    
 namespace QuotesSong
-{
 /// <summary>
 /// Class-helper for serialization of dictionary
 /// </summary>
     public class item
-    {
+    {     
         [XmlAttribute]
         public string id;
         [XmlAttribute]
